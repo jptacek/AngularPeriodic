be55b8a93e383f4bf495d51c08e61022914abef1
John Ptacek <jptacek@live.com>
2014-01-24 06:41:45 -0600

    Update with files for Lou Reed post

---
+++ Lou/js/louController2.js
@@ -0,0 +1,57 @@
+function lou2Ctrl($scope) {
+    $scope.lou2Drops = lou2Albums;
+}
+
+var lou2Albums = {
+    releases: [
+        {
+            "album": "Lou Reed",
+            "artist": "Lou Reed",
+            "releaseDate": "6/1/1972",
+            "image": "http://upload.wikimedia.org/wikipedia/en/8/88/Lour72.jpg",
+            "label": "RCA"
+        },
+        {
+            "album": "Transformer",
+            "artist": "Lou Reed",
+            "releaseDate": "11/8/1972",
+            "image": "http://upload.wikimedia.org/wikipedia/en/f/f1/xLoureedtransformer.jpeg",
+            "producer": "David Bowie, Mick Ronson",
+            "label": "RCA"
+        },
+        {
+            "album": "Berlin",
+            "artist": "Lou Reed",
+            "releaseDate": "7/1/1973",
+            "image": "http://upload.wikimedia.org/wikipedia/en/7/70/Berlinloureed.jpeg",
+            "producer": "Bob Ezrin",
+            "label": "RCA"
+        },
+        {
+            "album": "Sally Can't Dance",
+            "artist": "Lou Reed",
+            "releaseDate": "8/1/1974",
+            "image": "http://upload.wikimedia.org/wikipedia/en/6/66/SallyCD.jpg",
+            "producer": "Steve Katz, Lou Reed",
+            "label": "RCA"
+        },
+        {
+            "album": "Metal Machine Music",
+            "artist": "Lou Reed",
+            "releaseDate": "7/1/1975",
+            "image": "http://upload.wikimedia.org/wikipedia/en/6/65/xMetal_machine_music.jpg",
+            "producer": "Lou Reed",
+            "label": "RCA"
+        },
+        {
+            "album": "Coney Island Baby",
+            "artist": "Lou Reed",
+            "releaseDate": "12/1/1975",
+            "image": "http://upload.wikimedia.org/wikipedia/en/1/17/Reed_Coney.jpg",
+            "producer": "Lou Reed, Godfrey",
+            "label": "RCA"
+        },
+
+    ]
+};
+
