9bc81a07be0621dd6a7fbc1dd7b5df3b87830132
John Ptacek <jptacek@live.com>
2014-02-11 06:37:59 -0600

    Add code snippets for template

---
+++ Demo6-Template/js/chemistryController.js
@@ -0,0 +1,36 @@
+chemistryApp.controller('chemTemplateCtrl1',
+    function chemCtrl($scope) {
+        $scope.periodic = perioicData;
+
+        $scope.template =  {name: 'template1.html', url: 'js/templates/element1.html'};
+    }
+).controller('chemTemplateCtrl2',
+    function chemCtrl($scope) {
+        $scope.periodic = perioicData;
+
+        $scope.templates =
+            [ { name: 'template1.html', url: 'js/templates/element1.html'}
+                , { name: 'template2.html', url: 'js/templates/element2.html'}];
+        $scope.template = $scope.templates[1];
+
+    }
+).controller('chemTemplateCtrl3',
+    function chemCtrl($scope) {
+        $scope.periodic = perioicData;
+
+        $scope.templates =
+            [ { name: 'template1.html', url: 'js/templates/element1.html'}
+                , { name: 'template2.html', url: 'js/templates/element2.html'}
+                , { name: 'template3.html', url: 'js/templates/element3.html'}];
+        $scope.templateSelect = $scope.templates[0];
+
+        $scope.displayAll =  function() {
+            if ($scope.templateSelect.name == $scope.templates[2].name) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+);
