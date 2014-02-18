2057a9a49af9f7f927201d2b0158960a20c4f525
John Ptacek <jptacek@live.com>
2014-02-18 04:53:35 -0600

    Refactor demo2 structure

---
+++ Demo2/js/chemistryController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+chemistryApp.controller('chemistryController',
+    function chemistryController($scope) {
+        $scope.elementName = 'Hydrogen';
+        $scope.atomicNumber = 1;
+    }
+);
+
